de0bfa97014cd57cd3e0c03e06d66bc9cadaf28b
J Quintero <jfqs24@gmail.com>
2023-09-13 22:01:48 -0500

    Insert implementation

--- src/Demo.java
+++ src/Demo.java
@@ -3,7 +3,18 @@ import java.sql.*;
 public class Demo {
     public static void main(String[] args) {
 
+        String first_name = "Hector";
+        String second_name = "Ivan";
+        String first_lastname = "Figueroa";
+        String second_lastname = "";
+        String email = "hifdsc@hotmail.com";
         String basicQuery = "SELECT * FROM person LIMIT 20";
+        String addQuery = "INSERT INTO person (first_name, second_name, first_lastname, second_lastname, email) " +
+                          "VALUES ('" + first_name +
+                          "', '" + second_name +
+                          "', '" + first_lastname +
+                          "', '" + second_lastname +
+                          "', '" + email + "')";
         String url = "jdbc:mysql://localhost:3306/practice_db";
         String user = "root";
         String password = "my5q1P455w0rd";
@@ -14,6 +25,7 @@ public class Demo {
         try {
             connection = DriverManager.getConnection(url, user, password);
             statement = connection.createStatement();
+            statement.executeUpdate(addQuery);
             resultSet = statement.executeQuery(basicQuery);
             resultSet.next();
             System.out.println("ID : Name and Email");
@@ -28,7 +40,5 @@ public class Demo {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-
-
     }
 }
